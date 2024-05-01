69602277a06a1037de7b01e8f15716d324f55c56
zouzan_a <youzouzan_a@etna-alternance.net>
2024-05-01 22:28:51 +0200

    update alert && delete alert

--- coinapi/app.py
+++ coinapi/app.py
@@ -70,12 +70,62 @@ def get_alerts():
 
 
 # Update crypto alert
-@app.post("/alerts/<currency_id>")
+@app.put("/alerts/<currency_id>")
 def update_alert(currency_id):
-    return {"message": "Update alerts"}
+    args = request.json
+
+    with sqlite3.connect("database.db") as con:
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM alerts WHERE id=?", (currency_id,))
+        alert = cur.fetchone()
+
+        if 'name' in args and not isinstance(args['name'], str):
+            return ("The 'name' field must be a string.")
+        if 'currency' in args and not isinstance(args['currency'], str):
+            return ("The 'currency' field must be a string.")
+        if 'quote_currency' in args and not isinstance(args['quote_currency'], str):
+            return ("The 'quote_currency' field must be a string.")
+        if 'anchor_price' in args and not isinstance(args['anchor_price'], (int, float)):
+            return ("'anchor_price' field must be a number (int or float).")
+        if 'operation' in args and not isinstance(args['operation'], str):
+            return ("The 'operation' field must be a string.")
+
+        cur.execute(
+            """
+            UPDATE alerts
+            SET name=?, currency=?, quote_currency=?, anchor_price=?, operation=?,
+            WHERE id=?
+            """,
+            (
+                args.get('name', alert['name']),
+                args.get('currency', alert['currency']),
+                args.get('quote_currency', alert['quote_currency']),
+                args.get('anchor_price', alert['anchor_price']),
+                args.get('operation', alert['operation']),
+                currency_id
+            )
+        )
+
+        con.commit() 
+
+    return jsonify({"message": "Alert successfully updated!"})
 
 
 # Delete a crypto alert
 @app.delete("/alerts/<currency_id>")
 def delete_alert(currency_id):
-    return {"message": "Delete alert"}
+    with sqlite3.connect("database.db") as con:
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM alerts WHERE id=?", (currency_id,))
+        alert = cur.fetchone()
+
+        cur.execute("DELETE FROM alerts WHERE id=?", (currency_id,))
+        con.commit()
+
+    return jsonify({"message": "Alert successfully deleted!"})
+
+
+if __name__ == "__main__":
+    app.run()
